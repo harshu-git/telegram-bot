55a66274a89507dee6f32d6458eb9d4c7e26ec72
harshu-git <83920458+harshu-git@users.noreply.github.com>
2026-05-24 17:37:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import os
+import yt_dlp
 
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
+    MessageHandler,
     ContextTypes,
+    filters,
 )
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -13,8 +16,58 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(
-        "✅ Bot working!"
+        "🎬 Send YouTube video link"
     )
+
+
+async def download_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    url = update.message.text.strip()
+
+    msg = await update.message.reply_text(
+        "📥 Downloading..."
+    )
+
+    try:
+
+        if not os.path.exists("downloads"):
+            os.makedirs("downloads")
+
+        ydl_opts = {
+            "format": "mp4",
+            "outtmpl": "downloads/%(title)s.%(ext)s",
+            "quiet": True,
+            "noplaylist": True,
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
+            info = ydl.extract_info(url, download=True)
+
+            file_path = ydl.prepare_filename(info)
+
+        await msg.edit_text(
+            "📤 Uploading..."
+        )
+
+        with open(file_path, "rb") as video:
+
+            await update.message.reply_video(
+                video=video,
+                supports_streaming=True
+            )
+
+        os.remove(file_path)
+
+        await msg.edit_text(
+            "✅ Done!"
+        )
+
+    except Exception as e:
+
+        await msg.edit_text(
+            f"❌ Error:\n{str(e)}"
+        )
 
 
 def main():
@@ -24,9 +77,18 @@ def main():
 
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+        CommandHandler("start", start)
+    )
 
-    print("Bot started...")
+    app.add_handler(
+        MessageHandler(
+            filters.TEXT & ~filters.COMMAND,
+            download_video
+        )
+    )
+
+    print("Bot started successfully...")
 
     app.run_polling()
 
