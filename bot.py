5e1d7151c43556990e82ce9419b0d118d4816483
harshu-git <83920458+harshu-git@users.noreply.github.com>
2026-05-24 17:27:44 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,35 @@
+import os
+
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    await update.message.reply_text(
+        "✅ Bot is working!"
+    )
+
+
+def main():
+
+    if not BOT_TOKEN:
+        raise Exception("BOT_TOKEN missing!")
+
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot started successfully...")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
